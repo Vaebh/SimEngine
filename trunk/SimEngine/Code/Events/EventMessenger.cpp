e06b603fc1f81bcf9a43fdb252a2577054094352
simjmcd@gmail.com <simjmcd@gmail.com@f4ec5684-b7ad-7f83-a124-f7299fcd11b0>
2015-02-03 22:42:59 +0000

    Updated Event system to be a lot better

--- trunk/SimEngine/Code/Events/EventMessenger.cpp
+++ trunk/SimEngine/Code/Events/EventMessenger.cpp
@@ -1,36 +1,8 @@
 #include "../Events/EventMessenger.h"
 
-// TODO
-// Switch all the loops in all methods to work like the one in SubscribeToEvent - as I am dumb
-// and forgot that there were no pointers so I had to directly access and change the values
+EventMessenger* EventMessenger::mEventMessenger = NULL;
 
-// With that in mind will probably change mEvents to store pointers to Events, not just Events
-
-// Also have to work out how to pass class functions via function pointers, or if that's even possible without
-// making the function pointer specific to that class
-
-// Also look into making the enum list expandable, I want to be able to define more events outside of this header file via the
-// comments I put in EventMessenger.h. With that in mind I should change the delegate to take unsigned ints, not EventTypes
-
-EventMessenger* EventMessenger::mEventMessenger;
-
-namespace
-{
-	// Make a function for squashing the eventList down to size again
-}
-
-EventMessenger::EventMessenger()
-{
-	Event newEvent;
-
-	mEvents.reserve(NUM_EVENTS);
-	for(uint32_t i = 0; i < NUM_EVENTS; ++i)
-	{
-		newEvent.mEventType = EventType(i);
-
-		mEvents.push_back(newEvent);
-	}
-}
+EventMessenger::EventMessenger() {}
 
 EventMessenger* EventMessenger::GetSingleton()
 {
@@ -42,108 +14,61 @@ EventMessenger* EventMessenger::GetSingleton()
 	return mEventMessenger;
 }
 
-void EventMessenger::RecordEvent(const uint32_t& inEventType, GameObject* inTarget, const float& inEventNotificationDelay)
+std::vector<MessageDelegate> EventMessenger::GetEventDelegates(const uint32_t in_eventType, GameObject* in_target)
 {
-    for(uint32_t i = 0; i < mEvents.size(); ++i)
-	{
-		if(mEvents[i].mEventType == inEventType)
-		{
-			for(uint32_t j = 0; j < mEvents[i].mEventTargets.size(); ++j)
-			{
-                EventPair& theEventPair = mEvents[i].mEventTargets[j];
-                
-				// If the recorded gameobject matches the stored gameobject
-				if(theEventPair.first != NULL && theEventPair.first == inTarget)
-				{
-					(*theEventPair.second)(inEventType, theEventPair.first);
-					break;
-				}
-			}
-		}
-	}
+	std::pair<const uint32_t, GameObject*> keyPair = std::make_pair(in_eventType, in_target);
+	return m_eventMap[keyPair];
 }
 
-void EventMessenger::BroadcastEvent(const uint32_t& inEventType, const float& inEventNotificationDelay)
+// Fires the delegates for the triggered event
+void EventMessenger::RecordEvent(const uint32_t in_eventType, GameObject* in_target, const float in_eventNotificationDelay)
 {
-    for(uint32_t i = 0; i < mEvents.size(); ++i)
-	{
-		if(mEvents[i].mEventType == inEventType)
-		{
-            for(uint32_t j = 0; j < mEvents[i].mEventTargets.size(); ++j)
-			{
-				if(mEvents[i].mEventTargets[j].second != NULL)
-				{
-					(*mEvents[i].mEventTargets[j].second)(inEventType, NULL);
-				}
-			}
-		}
-	}
-}
-
-// Loop through event list, if the delegate is null or already there then return,
-// otherwise add it to the delegate list
-void EventMessenger::SubscribeToEvent(const uint32_t& inEventType, GameObject* inTarget, MessageDelegate inMsgDel)
-{
-	if(inMsgDel == NULL || inTarget == NULL)
+	if(in_target == NULL)
 		return;
 
-	for(uint32_t i = 0; i < mEvents.size(); ++i)
+	std::vector<MessageDelegate> delegates = GetEventDelegates(in_eventType, in_target);
+
+	for(int i = 0; i < delegates.size(); ++i)
 	{
-		if(mEvents[i].mEventType == inEventType)
-		{
-			for(uint32_t j = 0; j < mEvents[i].mEventTargets.size(); ++j)
-			{
-                EventPair& theEventPair = mEvents[i].mEventTargets[j];
-                
-				if(theEventPair.first == inTarget)
-				{
-					// If it's the same GameObject that we're subscribing to then just add the message delegate
-					// to that GameObjects vector of MessageDelegates, once it's implemented
-					//return;
-				}
-			}
-
-			mEvents[i].mEventTargets.push_back(std::make_pair(inTarget, inMsgDel));
-			return;
-		}
+		(*delegates[i])(in_eventType, in_target);
 	}
-
-	// If this event is not in the list then add it
-	Event newEvent;
-	newEvent.mEventType = inEventType;
-	newEvent.mEventTargets = std::vector<EventPair>();
-	newEvent.mEventTargets.push_back(std::make_pair(inTarget, inMsgDel));
-
-	mEvents.push_back(newEvent);
 }
 
-// TODO - Redo the unsubscribe function as it is currently not working, the capacity for the event vector just keeps going up and up and up
-
-
-// Loop through event list, if the delegate is null or not there then return,
-// otherwise remove it from the delegate list
-void EventMessenger::UnsubscribeToEvent(const uint32_t& inEventType, GameObject* inTarget, MessageDelegate inMsgDel)
+void EventMessenger::SubscribeToEvent(const uint32_t in_eventType, GameObject* in_target, MessageDelegate in_msgDel)
 {
-	if(inMsgDel == NULL || inTarget == NULL)
+	if(in_msgDel == NULL || in_target == NULL)
 		return;
 
-	for(uint32_t i = 0; i < mEvents.size(); ++i)
-	{
-		if(mEvents[i].mEventType == inEventType)
-		{
-            for(std::vector<EventPair>::iterator it = mEvents[i].mEventTargets.begin(); it != mEvents[i].mEventTargets.end(); ++it)
-			{
-                EventPair& theEventPair = *it;
-                
-				if(theEventPair.first == inTarget && theEventPair.second == inMsgDel)
-				{
-                    mEvents[i].mEventTargets.erase(it);
-					return;
-				}
-			}
+	std::pair<const uint32_t, GameObject*> keyPair = std::make_pair(in_eventType, in_target);
+	std::vector<MessageDelegate>& delegates = m_eventMap[keyPair];
 
-			// The delegate isn't here
+	for(int i = 0; i < delegates.size(); ++i)
+	{
+		if(delegates[i] == in_msgDel)
 			return;
+	}
+
+	delegates.push_back(in_msgDel);
+}
+
+
+void EventMessenger::UnsubscribeToEvent(const uint32_t in_eventType, GameObject* in_target, MessageDelegate in_msgDel)
+{
+	if(in_msgDel == NULL || in_target == NULL)
+		return;
+
+	std::pair<const uint32_t, GameObject*> keyPair = std::make_pair(in_eventType, in_target);
+	std::vector<MessageDelegate>& delegates = m_eventMap[keyPair];
+
+	for(int i = 0; i < delegates.size(); ++i)
+	{
+		if(delegates[i] == in_msgDel)
+		{
+			delegates.erase(delegates.begin()+i);
+			break;
 		}
 	}
+
+	if(delegates.empty())
+		m_eventMap.erase(keyPair);
 }
--- trunk/SimEngine/Code/Events/EventMessenger.h
+++ trunk/SimEngine/Code/Events/EventMessenger.h
@@ -4,195 +4,33 @@
 #include "../Structure/Component.h"
 #include "../Foundation/Foundation.h"
 #include "../Events/EventCallback.h"
+#include "../Events/EventTypes.h"
 #include <utility>
-
-// The main problem with this system right now is there's no way to subscribe the events of specific components or gameobjects
-// For example I could want to listen to the collision event of a specific BounceComponent, the one attached to the ball in a Breakout game
-// However all I can do right now is listen for ALL collision events
-
-// One way is to pass in pointers to specific components, but that constrains the events so they can only be used in components, not anywhere I please
-
-// Really the issue is that I want to listen to the things (events) that occur to specific things
-// So I want to listen to the OnCollision event of the ball, but nothing else
-// The only way I can think of to do that would be to store a pointer to the object that I want to listen to
-
-// TODO - add the numpad keys
-
-enum EventType
-{
-	COLLISION,
-	DEATH,
-
-	INPUT_1_PRESS,
-	INPUT_1_RELEASE,
-	INPUT_2_PRESS,
-	INPUT_2_RELEASE,
-	INPUT_3_PRESS,
-	INPUT_3_RELEASE,
-	INPUT_4_PRESS,
-	INPUT_4_RELEASE,
-	INPUT_5_PRESS,
-	INPUT_5_RELEASE,
-	INPUT_6_PRESS,
-	INPUT_6_RELEASE,
-	INPUT_7_PRESS,
-	INPUT_7_RELEASE,
-	INPUT_8_PRESS,
-	INPUT_8_RELEASE,
-	INPUT_9_PRESS,
-	INPUT_9_RELEASE,
-	INPUT_0_PRESS,
-	INPUT_0_RELEASE,
-
-	INPUT_MINUS_PRESS,
-	INPUT_MINUS_RELEASE,
-	INPUT_EQUAL_PRESS,
-	INPUT_EQUAL_RELEASE,
-	INPUT_BACKSPACE_PRESS,
-	INPUT_BACKSPACE_RELEASE,
-
-	INPUT_Q_PRESS,
-	INPUT_Q_RELEASE,
-	INPUT_W_PRESS,
-	INPUT_W_RELEASE,
-	INPUT_E_PRESS,
-	INPUT_E_RELEASE,
-	INPUT_R_PRESS,
-	INPUT_R_RELEASE,
-
-	INPUT_T_PRESS,
-	INPUT_T_RELEASE,
-	INPUT_Y_PRESS,
-	INPUT_Y_RELEASE,
-	INPUT_U_PRESS,
-	INPUT_U_RELEASE,
-	INPUT_I_PRESS,
-	INPUT_I_RELEASE,
-
-	INPUT_O_PRESS,
-	INPUT_O_RELEASE,
-	INPUT_P_PRESS,
-	INPUT_P_RELEASE,
-	INPUT_LEFT_BRACKET_PRESS,
-	INPUT_LEFT_BRACKET_RELEASE,
-	INPUT_RIGHT_BRACKET_PRESS,
-	INPUT_RIGHT_BRACKET_RELEASE,
-	INPUT_ENTER_PRESS,
-	INPUT_ENTER_RELEASE,
-	
-	INPUT_A_PRESS,
-	INPUT_A_RELEASE,
-	INPUT_S_PRESS,
-	INPUT_S_RELEASE,
-	INPUT_D_PRESS,
-	INPUT_D_RELEASE,
-	INPUT_F_PRESS,
-	INPUT_F_RELEASE,
-
-	INPUT_G_PRESS,
-	INPUT_G_RELEASE,
-	INPUT_H_PRESS,
-	INPUT_H_RELEASE,
-	INPUT_J_PRESS,
-	INPUT_J_RELEASE,
-	INPUT_K_PRESS,
-	INPUT_K_RELEASE,
-
-	INPUT_L_PRESS,
-	INPUT_L_RELEASE,
-	INPUT_SEMICOLON_PRESS,
-	INPUT_SEMICOLON_RELEASE,
-	INPUT_APOSTROPHE_PRESS,
-	INPUT_APOSTROPHE_RELEASE,
-	
-	INPUT_LEFT_SHIFT_PRESS,
-	INPUT_LEFT_SHIFT_RELEASE,
-	INPUT_BACKSLASH_PRESS,
-	INPUT_BACKSLASH_RELEASE,
-	INPUT_Z_PRESS,
-	INPUT_Z_RELEASE,
-	INPUT_X_PRESS,
-	INPUT_X_RELEASE,
-	INPUT_C_PRESS,
-	INPUT_C_RELEASE,
-
-	INPUT_V_PRESS,
-	INPUT_V_RELEASE,
-	INPUT_B_PRESS,
-	INPUT_B_RELEASE,
-	INPUT_N_PRESS,
-	INPUT_N_RELEASE,
-	INPUT_M_PRESS,
-	INPUT_M_RELEASE,
-
-	INPUT_COMMA_PRESS,
-	INPUT_COMMA_RELEASE,
-	INPUT_PERIOD_PRESS,
-	INPUT_PERIOD_RELEASE,
-	INPUT_SLASH_PRESS,
-	INPUT_SLASH_RELEASE,
-	INPUT_RIGHT_SHIFT_PRESS,
-	INPUT_RIGHT_SHIFT_RELEASE,
-
-	INPUT_LEFT_CONTROL_PRESS,
-	INPUT_LEFT_CONTROL_RELEASE,
-	INPUT_LEFT_ALT_PRESS,
-	INPUT_LEFT_ALT_RELEASE,
-	INPUT_SPACE_PRESS,
-	INPUT_SPACE_RELEASE,
-	INPUT_RIGHT_ALT_PRESS,
-	INPUT_RIGHT_ALT_RELEASE,
-	INPUT_RIGHT_CONTROL_PRESS,
-	INPUT_RIGHT_CONTROL_RELEASE,
-
-	INPUT_LEFT_PRESS,
-	INPUT_LEFT_RELEASE,
-	INPUT_DOWN_PRESS,
-	INPUT_DOWN_RELEASE,
-	INPUT_RIGHT_PRESS,
-	INPUT_RIGHT_RELEASE,
-	INPUT_UP_PRESS,
-	INPUT_UP_RELEASE,
-
-	INPUT_LEFT_CLICK_PRESS,
-	INPUT_LEFT_CLICK_RELEASE,
-	INPUT_RIGHT_CLICK_PRESS,
-	INPUT_RIGHT_CLICK_RELEASE,
-
-	NUM_EVENTS
-};
+#include <map>
 
 typedef IEventCallback* MessageDelegate;
-typedef std::pair<GameObject*, MessageDelegate> EventPair;
-
-// Should maybe be a map of pairs - pair<EventID id, GameObject* target>, with the data being a vector of message delegates
-// This might make it more difficult to send a complete event message when it comes time to do networking
-// Should at least be a collection of message delegates per GameObject* as the same object could have multiple things subscribed to it
-struct Event
-{
-	uint32_t mEventType;
-	std::vector<EventPair> mEventTargets;
-};
 
 class EventMessenger
 {
-public:
 	EventMessenger();
 
+public:
 	static EventMessenger* GetSingleton();
 
 	// Will have to add another parameter for passing event specific info, an array of string or uint params maybe
-	void RecordEvent(const uint32_t& inEventType, GameObject* inTarget, const float& inEventNotificationDelay = 0.f);
-	void BroadcastEvent(const uint32_t& inEventType, const float& inEventNotificationDelay = 0.f);
+	void RecordEvent(const uint32_t in_eventType, GameObject* in_target, const float in_eventNotificationDelay = 0.f);
 
-	void SubscribeToEvent(const uint32_t& inEventType, GameObject* inTarget, MessageDelegate inMsgDel);
-	void UnsubscribeToEvent(const uint32_t& inEventType, GameObject* inTarget, MessageDelegate inMsgDel);
+	void SubscribeToEvent(const uint32_t in_eventType, GameObject* in_target, MessageDelegate in_msgDel);
+	void UnsubscribeToEvent(const uint32_t in_eventType, GameObject* in_target, MessageDelegate in_msgDel);
 
 	// TODO - Add delayed events, will likely need an event queue
 	//void Update(float inDT);
 
 private:
-	std::vector<Event> mEvents;
+	std::vector<MessageDelegate> GetEventDelegates(const uint32_t in_eventType, GameObject* in_target);
+
+private:
+	std::map< std::pair<const uint32_t, GameObject*>, std::vector<MessageDelegate> > m_eventMap;
 
 	static EventMessenger* mEventMessenger;
 };
