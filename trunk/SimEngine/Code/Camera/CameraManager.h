22b128af016dae3e3aef8e3726b51b20df430de4
simjmcd@gmail.com <simjmcd@gmail.com@f4ec5684-b7ad-7f83-a124-f7299fcd11b0>
2015-02-03 22:42:28 +0000

    Added first draft of Camera systems

---
+++ trunk/SimEngine/Code/Camera/CameraManager.h
@@ -0,0 +1,29 @@
+#ifndef CAM_MANAGER_SIMENGINE
+#define CAM_MANAGER_SIMENGINE
+
+#include "../Camera/Camera.h"
+#include "../Foundation/Foundation.h"
+
+#include <vector>
+
+class CameraManager
+{
+public:
+	CameraManager();
+	CameraManager(Camera in_defaultCamera);
+
+	const int AddCamera(Camera in_newCamera, bool in_makeActive = false);
+
+	const Camera* GetActiveCamera() const;
+	const Camera* GetCameraAtIndex(int in_camIndex) const;
+
+	void SetActiveCamera(const int in_index);
+
+private:
+	int m_activeCamIndex;
+
+	// Might use a stack for this actually
+	std::vector<Camera*> m_cameraVector;
+};
+
+#endif
