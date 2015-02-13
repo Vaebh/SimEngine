f178c3b4618871575eb94a0b119206b81c361e82
simjmcd@gmail.com <simjmcd@gmail.com@f4ec5684-b7ad-7f83-a124-f7299fcd11b0>
2015-02-13 03:32:03 +0000

    - Fleshed out Buffer class more - Created VertexAttribute class

---
+++ SimEngine/Code/Rendering/VertexAttribute.h
@@ -0,0 +1,24 @@
+#ifndef VERTEX_ATTRIBUTE_SIMENGINE
+#define VERTEX_ATTRIBUTE_SIMENGINE
+
+#include "../OpenGL/GLIncludes.h"
+
+class VertexAttribute
+{
+public:
+	VertexAttribute();
+	VertexAttribute(GLuint in_index, GLint in_size, GLenum in_type, GLboolean in_normalised, GLsizei in_stride, GLvoid* in_offset);
+
+	void Enable(GLuint in_index);
+	void Set();
+
+private:
+	GLuint m_index;
+	GLint m_size;
+	GLenum m_type;
+	GLboolean m_normalised;
+	GLsizei m_stride;
+	GLvoid* m_offset;
+};
+
+#endif
